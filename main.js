aa2dfb9319e2046e01333c98dbf0ca63b9555ccc
Andras Toth <andras.toth@lightware.com>
2025-12-01 17:45:31 +0100

    New MMU Node tree format

--- main.js
+++ main.js
@@ -47,8 +47,7 @@ if (networkAddresses.length > 0) {
 
 console.log('='.repeat(50));
 
-const layers = ['VIDEO','AUDIO','USBICRON','USBHID'];
-const roomName = 'MAIN';
+const layers = ['VIDEO','AUDIO','USBICRON','USBHID']
 
 // Generic nodes:
 const genericTxNode = {
@@ -61,38 +60,52 @@ const genericRxNode = {
     SourceStream: 0
 };
 
+const matrixSize = 10; // 10x10 matrix
 
-
-
-// Initializing nodes (1-3):
+// Initializing nodes:
 layers.forEach(layer => {
-    for (let id=1; id<=10; id++) {
-        server.V1.TPNAPP.MEDIA[layer][`${roomName}_TX${id}_S0`] = genericTxNode;
-        server.V1.TPNAPP.MEDIA[layer][`${roomName}_RX${id}_D0`] = genericRxNode;
-        // Create Symlinked nodes under XP as well
-        server.V1.TPNAPP.MEDIA[layer].XP[`${roomName}_TX${id}_S0`] = server.V1.TPNAPP.MEDIA[layer][`${roomName}_TX${id}_S0`];
-        server.V1.TPNAPP.MEDIA[layer].XP[`${roomName}_RX${id}_D0`] = server.V1.TPNAPP.MEDIA[layer][`${roomName}_RX${id}_D0`];       
-        // Set RX SourceStream to Read/Write
-        server.V1.TPNAPP.MEDIA[layer][`${roomName}_RX${id}_D0`].SourceStream__rw__ = true;
+    for (let id=1; id<=matrixSize; id++) {
+        // generate random MAC for each device 
+        let macAddress = `${Math.random().toString(16).slice(2, 8).toUpperCase()}`;
+        server.V1.MEDIA[layer][`${macAddress}_TX${id}_S0`] = genericTxNode;
+        server.V1.MEDIA[layer][`${macAddress}_TX${id}_S0`].StreamName = `TX_${id}_S0`;
         
-        // set Enabled property to R/W
-        server.V1.TPNAPP.MEDIA[layer][`${roomName}_TX${id}_S0`].Enabled__rw__ = true;
+        macAddress = `${Math.random().toString(16).slice(2, 8).toUpperCase()}`;
+        server.V1.MEDIA[layer][`${macAddress}_RX${id}_D0`] = genericRxNode;
+        server.V1.MEDIA[layer][`${macAddress}_RX${id}_D0`].StreamName = `RX_${id}_S0`;
+
+        // Create Symlinked nodes under XP as well
+        server.V1.MEDIA[layer].XP[`${macAddress}_TX${id}_S0`] = server.V1.MEDIA[layer][`${macAddress}_TX${id}_S0`];
+        server.V1.MEDIA[layer].XP[`${macAddress}_RX${id}_D0`] = server.V1.MEDIA[layer][`${macAddress}_RX${id}_D0`];
+  
+        // Set RX SourceStream to Read/Write
+        server.V1.MEDIA[layer][`${macAddress}_RX${id}_D0`].SourceStream__rw__ = true;
 
         // only for testing purposes, allow TX SignalPresent to be set manually
-        server.V1.TPNAPP.MEDIA[layer][`${roomName}_TX${id}_S0`].SignalPresent__rw__ = true;
+        server.V1.MEDIA[layer][`${macAddress}_TX${id}_S0`].SignalPresent__rw__ = true;
         
         // Implement the XP switch command
-        server.V1.TPNAPP.MEDIA[layer].XP = {
+        server.V1.MEDIA[layer].XP = {
             switch: (command) => handleXPSwitch(layer, command)
         };
 
         // If any TX signal present is changed then the connected RXs (can be multiple) signal present propoerties are to be updated accordingly.
-        server.V1.TPNAPP.MEDIA[layer][`${roomName}_TX${id}_S0`].on('SignalPresent', (path, prop, value) => {
+        server.V1.MEDIA[layer][`${macAddress}_TX${id}_S0`].on('SignalPresent', (path, prop, value) => {
             handleTxSignalPresentChange(layer, path, prop, value);
         });
     }
 });
 
+// Find the node of the layer that contains the given StreamName
+const findNodeByStreamName = (layer, streamName) => {
+    const mediaJson = server.V1.MEDIA[layer].toJSON();
+    for (const [nodeName, node] of Object.entries(mediaJson)) {
+        if (node.StreamName === streamName) {
+            return nodeName;
+        }
+    }
+    return null;
+};
 
 // Function to handle XP switch command
 const handleXPSwitch = (layer, command) => {
@@ -108,28 +121,31 @@ const handleXPSwitch = (layer, command) => {
     // Get source and destination from command
     const [src, dest] = command.split(':');
 
-    // the server.V1.TPNAPP.MEDIA[layer].toJson() returns with the devices as nodes,
-    // validate whether the source and destination exist
-    // using this JSON representation to avoid issues with references
-    const mediaJson = server.V1.TPNAPP.MEDIA[layer].toJSON();
-    if (!mediaJson.hasOwnProperty(src)) {
-        throw new Error(`Invalid source stream: ${src}`);
-    }
-    if (!mediaJson.hasOwnProperty(dest)) {
-        throw new Error(`Invalid destination stream: ${dest}`);
+    // Check if the source exists among the "_S0" as StreamName in the layer
+    const srcNodeName = findNodeByStreamName(layer, src);
+    if (!srcNodeName || !srcNodeName?.endsWith('_S0')) {
+        throw new Error(`Source stream ${src} not found in layer ${layer}`);
     }
 
+    // Check if the destination exists among the "_D0" as StreamName in the layer
+    const destNodeName = findNodeByStreamName(layer, dest);
+    if (!destNodeName || !destNodeName?.endsWith('_D0')) {
+        throw new Error(`Destination stream ${dest} not found in layer ${layer}`);
+    }
+
+    
+
     // If source stream is not enabled, throw exception
-    if (!server.V1.TPNAPP.MEDIA[layer][src].Enabled) {
+    if (!server.V1.MEDIA[layer][src].Enabled) {
         throw new Error(`Source stream ${src} is not enabled`);
     }
 
     // OK, now we can set the SourceStream of the destination to the source
-    server.V1.TPNAPP.MEDIA[layer][dest].SourceStream = src;
+    server.V1.MEDIA[layer][dest].SourceStream = src;
 
     // If signal is present on the source, set it on the destination as well
-    const signalPresent = server.V1.TPNAPP.MEDIA[layer][src].SignalPresent;
-    server.V1.TPNAPP.MEDIA[layer][dest].SignalPresent = signalPresent;
+    const signalPresent = server.V1.MEDIA[layer][src].SignalPresent;
+    server.V1.MEDIA[layer][dest].SignalPresent = signalPresent;
 };
 
 // Function to handle TX SignalPresent changes and propagate to connected RXs
@@ -138,10 +154,10 @@ const handleTxSignalPresentChange = (layer, path, prop, value) => {
     // Get the TX name from the path like "/V1/TPNAPP/MEDIA/VIDEO/MAIN_TX1_S0"
     // txStreamName is like MAIN_TX1_S0
     const txStreamName = path.split('/').pop();
-    // console.log(`SignalPresent changed on ${txStreamName} to ${value}`);
+    console.log(`SignalPresent changed on ${txStreamName} to ${value}`);
 
-    Object.keys(server.V1.TPNAPP.MEDIA[layer]).forEach(nodeName => {
-        const node = server.V1.TPNAPP.MEDIA[layer][nodeName];
+    Object.keys(server.V1.MEDIA[layer]).forEach(nodeName => {
+        const node = server.V1.MEDIA[layer][nodeName];
         // console.log(`Checking node ${nodeName} with SourceStream ${node.SourceStream}`);
         if (nodeName.startsWith(`${roomName}_RX`) && node.SourceStream === txStreamName) {
             console.log(`Updating SignalPresent on ${nodeName} to ${value}`);
